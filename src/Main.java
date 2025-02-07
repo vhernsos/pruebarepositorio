603313cc1a14c6bb0f8feb9370dad78b8b88567c
vhernsos <vhernsos@myuax.com>
2025-02-07 10:38:49 +0100

    Cambios varios

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("HOLA Y BIENVENIDOS");
+        int numero1 = 2;
+        String hola = "hola";
+        System.out.println("El numero es: " + numero1 + " y la palabra es: " + hola);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner escaneo = new Scanner(System.in);
+        System.out.println("Ingrese el nombre: ");
+        int numero = escaneo.nextInt();
+        System.out.println("El numero es " + numero);
+
+        if (numero%2 == 0){
+            System.out.println("El numero " + numero  + " es par");
+        } else
+            System.out.println("El numero " + numero  + " es impar");
     }
 }
